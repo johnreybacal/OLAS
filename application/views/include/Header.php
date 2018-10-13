a972047eb24373c1c64ae55d7ecb320257659e33
naytanyel <ncpiquero@gmail.com>
2018-10-13 19:36:12 +0800

    Fixed Swal
    Removed swal on assetsOLAS folder.
    Called swal.all.min.js

--- application/views/include/Header.php
+++ application/views/include/Header.php
@@ -7,18 +7,14 @@
 
 	<title>OLAS</title>
 
-	<link href="<?php echo base_url('assets/css/core.min.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/css/core.min.css' ); ?>" rel="stylesheet">
 	<link href="<?php echo base_url('assets/css/app.min.css'); ?>" rel="stylesheet">
 	<link href="<?php echo base_url('assets/css/style.min.css'); ?>" rel="stylesheet">
 
-	<link href="<?php echo base_url('assetsOLAS/swal/dist/sweetalert2.min.css'); ?>" rel="stylesheet">
-
 	<script src = "<?php echo base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>	
 	<script src = "<?php echo base_url('assetsOLAS/js/marc-reader.js'); ?>"></script>
-	
-	<script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
-	<script src = "<?php echo base_url('assetsOLAS/swal/dist/sweetalert2.all.min.js'); ?>"></script>
-	<script src = "<?php echo base_url('assetsOLAS/swal/dist/sweetalert2.min.js'); ?>"></script>
+
+	<script src = "<?php echo base_url('assets/vendor/sweetalert2/sweetalert2.all.min.js'); ?>"></script>
 	
 </head>
 
