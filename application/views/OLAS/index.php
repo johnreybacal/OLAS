ae46b99d5946bd1eabfc7f8b98034a28d6a0d1ab
Nath Piquero <30515378+naytanyel@users.noreply.github.com>
2018-10-17 16:29:25 +0900

    Update application/views/OLAS/index.php

--- application/views/OLAS/index.php
+++ application/views/OLAS/index.php
@@ -1,4 +1,149 @@
 <div class="main-content">
+        <div class="row">
+        <div class=" row gap-2 gap-y col-md-8 col-lg-8 col-sm-12" style="border: 3px solid blue;">
+              <div class=" hover-shadow-2  col-md-6 col-lg-6 col-sm-12">
+            <a class="media bg-white align-items-center hover-shadow-2 transition-5s" href="article-introduction.html">
+              <div class="media-left" style="border: 2px red solid;">
+                <!-- <i class="fa fa-file-text-o fs-35 text-secondary"></i> -->
+                  <img class="w-80px h-80px" src="../assets/img/login-bg.jpg" alt="...">
+                
+              </div>
+
+              <div class="media-body">
+                <p class="lead fw-400">Introduction</p>
+                <p><small>Article</small></p>
+              </div>
+
+              <!-- <div class="media-footer"><i class="ti-angle-right text-primary"></i></div> -->
+            </a>
+          </div>
+          <div class=" hover-shadow-2  col-md-6 col-lg-6 col-sm-12">
+            <a class="media bg-white align-items-center hover-shadow-2 transition-5s" href="article-introduction.html">
+              <div class="media-left" style="border: 2px red solid;">
+                <!-- <i class="fa fa-file-text-o fs-35 text-secondary"></i> -->
+                  <img class="w-80px h-80px" src="../assets/img/login-bg.jpg" alt="...">
+                
+              </div>
+
+              <div class="media-body">
+                <p class="lead fw-400">Introduction</p>
+                <p><small>Article</small></p>
+              </div>
+
+              <!-- <div class="media-footer"><i class="ti-angle-right text-primary"></i></div> -->
+            </a>
+          </div>
+          <div class=" hover-shadow-2  col-md-6 col-lg-6 col-sm-12">
+            <a class="media bg-white align-items-center hover-shadow-2 transition-5s" href="article-introduction.html">
+              <div class="media-left" style="border: 2px red solid;">
+                <!-- <i class="fa fa-file-text-o fs-35 text-secondary"></i> -->
+                  <img class="w-80px h-80px" src="../assets/img/login-bg.jpg" alt="...">
+                
+              </div>
+
+              <div class="media-body">
+                <p class="lead fw-400">Introduction</p>
+                <p><small>Article</small></p>
+              </div>
+
+              <!-- <div class="media-footer"><i class="ti-angle-right text-primary"></i></div> -->
+            </a>
+          </div>
+          <div class=" hover-shadow-2  col-md-6 col-lg-6 col-sm-12">
+            <a class="media bg-white align-items-center hover-shadow-2 transition-5s" href="article-introduction.html">
+              <div class="media-left" style="border: 2px red solid;">
+                <!-- <i class="fa fa-file-text-o fs-35 text-secondary"></i> -->
+                  <img class="w-80px h-80px" src="../assets/img/login-bg.jpg" alt="hayop">
+                
+              </div>
+
+              <div class="media-body">
+                <p class="lead fw-400">Introduction</p>
+                <p><small>Article</small></p>
+              </div>
+
+              <!-- <div class="media-footer"><i class="ti-angle-right text-primary"></i></div> -->
+            </a>
+          </div>
+              <!-- <div class="col-6">
+                <div class="h-40px b-1 bg-pale-secondary">1</div>
+              </div>
+
+              <div class="col-6">
+                <div class="h-40px b-1 bg-pale-secondary">2</div>
+              </div>
+
+              <div class="col-6">
+                <div class="h-40px b-1 bg-pale-secondary">3</div>
+              </div>
+
+              <div class="col-6">
+                <div class="h-40px b-1 bg-pale-secondary">4</div>
+              </div>
+
+              <div class="col-6">
+                <div class="h-40px b-1 bg-pale-secondary">5</div>
+              </div>
+
+              <div class="col-6">
+                <div class="h-40px b-1 bg-pale-secondary">6</div>
+              </div> -->
+        </div>
+        <div class=" col-md-4 col-md-4" style="border: 3px solid red;">
+              <div class="media media-single" style="border: 3px solid purple;">
+                <img class="avatar" src="../assets/img/avatar/1.jpg" alt="...">
+                <a class="title hover-primary text-truncate" href="#"><strong>Kermit D. Larson, John J. Wild</strong></a>
+                <a class="media-action hover-primary" href="#" data-provide="tooltip" title="Edit"><i class="ti-pencil"></i></a>
+                <a class="media-action hover-danger" href="#" data-provide="tooltip" title="Delete"><i class="ti-close"></i></a>
+              </div>
+              <div class="media media-single">
+                <img class="avatar" src="../assets/img/avatar/1.jpg" alt="...">
+                <a class="title hover-primary" href="#"><strong>Kermit D. </strong></a>
+                <a class="media-action hover-primary" href="#" data-provide="tooltip" title="Edit"><i class="ti-pencil"></i></a>
+                <a class="media-action hover-danger" href="#" data-provide="tooltip" title="Delete"><i class="ti-close"></i></a>
+              </div>
+              <div class="media media-single">
+                <img class="avatar" src="../assets/img/avatar/1.jpg" alt="...">
+                <a class="title hover-primary" href="#"><strong>Kermit D. Larson, John J. Wild</strong></a>
+                <a class="media-action hover-primary" href="#" data-provide="tooltip" title="Edit"><i class="ti-pencil"></i></a>
+                <a class="media-action hover-danger" href="#" data-provide="tooltip" title="Delete"><i class="ti-close"></i></a>
+              </div>
+              <div class="media media-single">
+                <img class="avatar" src="../assets/img/avatar/1.jpg" alt="...">
+                <a class="title hover-primary" href="#"><strong>Maryam Amiri</strong></a>
+                <a class="media-action hover-primary" href="#" data-provide="tooltip" title="Edit"><i class="ti-pencil"></i></a>
+                <a class="media-action hover-danger" href="#" data-provide="tooltip" title="Delete"><i class="ti-close"></i></a>
+              </div>
+              <div class="media media-single">
+                <img class="avatar" src="../assets/img/avatar/1.jpg" alt="...">
+                <a class="title hover-primary" href="#"><strong>Maryam Amiri</strong></a>
+                <a class="media-action hover-primary" href="#" data-provide="tooltip" title="Edit"><i class="ti-pencil"></i></a>
+                <a class="media-action hover-danger" href="#" data-provide="tooltip" title="Delete"><i class="ti-close"></i></a>
+              </div>
+              <!-- <div class="col-12">
+                <div class="h-40px b-1 bg-pale-secondary"></div>
+              </div>
+
+              <div class="col-12">
+                <div class="h-40px b-1 bg-pale-secondary"></div>
+              </div>
+
+              <div class="col-12">
+                <div class="h-40px b-1 bg-pale-secondary"></div>
+              </div>
+
+              <div class="col-12">
+                <div class="h-40px b-1 bg-pale-secondary"></div>
+              </div>
+
+              <div class="col-12">
+                <div class="h-40px b-1 bg-pale-secondary"></div>
+              </div>
+
+              <div class="col-12">
+                <div class="h-40px b-1 bg-pale-secondary"></div>
+              </div> -->
+        </div>
 	<div class="card">
         <div class="card-body">
             <h1>Welcome to OLAS!</h1>
