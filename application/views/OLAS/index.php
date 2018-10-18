ca6ef3e083a030cc4ca66dd968b5bc1e511f86e8
Nath Piquero <30515378+naytanyel@users.noreply.github.com>
2018-10-18 14:37:45 +0900

    Olas Index

--- application/views/OLAS/index.php
+++ application/views/OLAS/index.php
@@ -1,149 +1,145 @@
 <div class="main-content">
-        <div class="row">
-        <div class=" row gap-2 gap-y col-md-8 col-lg-8 col-sm-12" style="border: 3px solid blue;">
-              <div class=" hover-shadow-2  col-md-6 col-lg-6 col-sm-12">
-            <a class="media bg-white align-items-center hover-shadow-2 transition-5s" href="article-introduction.html">
-              <div class="media-left" style="border: 2px red solid;">
-                <!-- <i class="fa fa-file-text-o fs-35 text-secondary"></i> -->
-                  <img class="w-80px h-80px" src="../assets/img/login-bg.jpg" alt="...">
-                
-              </div>
+    <div class="row">
+        <div class="col-8">
+            <div class="card">
+                <h4 class="card-title">Recent Books</h4>
+                <div class="media-list media-list-divided media-list-hover">
+                    <div class="media">
+                      <!-- <span class="avatar status-success"> -->
+                        <img class="w-80px h-80px" src="../assetsOlas/img/login.jpg" alt="..." style="border: 2px red solid">
+                      <!-- </span> -->
+                      <div class="media-body">
+                        <p>
+                          <strong>BBQ this weekend?</strong>
+                          <!-- <time class="float-right" datetime="2018-07-14 20:00">24 min ago</time> -->
+                        </p>
+                        <p class="fs-14 semibold">Maryam Amiri</p>
+                        <p>I'll be in your neightborhood doing errands this weekend. Do you want me bring anything?</p>
+                      </div>
+                      <a class="media-action hover-primary" href="#" data-provide="tooltip" title="Edit"><i class="ti-pencil"></i></a>
+                      <a class="media-action hover-danger" href="#" data-provide="tooltip" title="Delete"><i class="ti-close"></i></a>
+                    </div>
+                    <div class="media">
+                      <!-- <span class="avatar status-success"> -->
+                        <img class="w-80px h-80px" src="../assets/img/avatar/1.jpg" alt="..." style="border: 2px red solid">
+                      <!-- </span> -->
+                      <div class="media-body">
+                        <p>
+                          <strong>BBQ this weekend?</strong>
+                          <!-- <time class="float-right" datetime="2018-07-14 20:00">24 min ago</time> -->
+                        </p>
+                        <p class="fs-14 semibold">Maryam Amiri</p>
+                        <p>I'll be in your neightborhood doing errands this weekend. Do you want me bring anything?</p>
+                      </div>
+                    </div>
+                    <div class="media">
+                      <!-- <span class="avatar status-success"> -->
+                        <img class="w-80px h-80px" src="../assets/img/avatar/1.jpg" alt="..." style="border: 2px red solid">
+                      <!-- </span> -->
+                      <div class="media-body">
+                        <p>
+                          <strong>BBQ this weekend?</strong>
+                          <!-- <time class="float-right" datetime="2018-07-14 20:00">24 min ago</time> -->
+                        </p>
+                        <p class="fs-14 semibold">Maryam Amiri</p>
+                        <p>I'll be in your neightborhood doing errands this weekend. Do you want me bring anything?</p>
+                      </div>
+                    </div>
+                    <div class="media">
+                      <!-- <span class="avatar status-success"> -->
+                        <img class="w-80px h-80px" src="../assets/img/avatar/1.jpg" alt="..." style="border: 2px red solid">
+                      <!-- </span> -->
+                      <div class="media-body">
+                        <p>
+                          <strong>BBQ this weekend?</strong>
+                          <!-- <time class="float-right" datetime="2018-07-14 20:00">24 min ago</time> -->
+                        </p>
+                        <p class="fs-14 semibold">Maryam Amiri</p>
+                        <p>I'll be in your neightborhood doing errands this weekend. Do you want me bring anything?</p>
+                      </div>
+                    </div>
+                    <div class="media">
+                      <!-- <span class="avatar status-success"> -->
+                        <img class="w-80px h-80px" src="../assets/img/avatar/1.jpg" alt="..." style="border: 2px red solid">
+                      <!-- </span> -->
+                      <div class="media-body">
+                        <p>
+                          <strong>BBQ this weekend?</strong>
+                          <!-- <time class="float-right" datetime="2018-07-14 20:00">24 min ago</time> -->
+                        </p>
+                        <p class="fs-14 semibold">Maryam Amiri</p>
+                        <p>I'll be in your neightborhood doing errands this weekend. Do you want me bring anything?</p>
+                      </div>
+                    </div>
+                    <div class="media">
+                      <!-- <span class="avatar status-success"> -->
+                        <img class="w-80px h-80px" src="../assets/img/avatar/1.jpg" alt="..." style="border: 2px red solid">
+                      <!-- </span> -->
+                      <div class="media-body">
+                        <p>
+                          <strong>BBQ this weekend?</strong>
+                          <!-- <time class="float-right" datetime="2018-07-14 20:00">24 min ago</time> -->
+                        </p>
+                        <p class="fs-14 semibold">Maryam Amiri</p>
+                        <p>I'll be in your neightborhood doing errands this weekend. Do you want me bring anything?</p>
+                      </div>
+                    </div>
 
-              <div class="media-body">
-                <p class="lead fw-400">Introduction</p>
-                <p><small>Article</small></p>
-              </div>
+               <!--  <div class="media media-single col-6">
+                  <img class="avatar avatar-sm" src="../assets/img/avatar/2.jpg" alt="...">
+                  <span>Hossein Shams</span>
+                </div>
 
-              <!-- <div class="media-footer"><i class="ti-angle-right text-primary"></i></div> -->
-            </a>
-          </div>
-          <div class=" hover-shadow-2  col-md-6 col-lg-6 col-sm-12">
-            <a class="media bg-white align-items-center hover-shadow-2 transition-5s" href="article-introduction.html">
-              <div class="media-left" style="border: 2px red solid;">
-                <!-- <i class="fa fa-file-text-o fs-35 text-secondary"></i> -->
-                  <img class="w-80px h-80px" src="../assets/img/login-bg.jpg" alt="...">
-                
-              </div>
+                <div class="media media-single">
+                  <img class="avatar avatar-sm" src="../assets/img/avatar/3.jpg" alt="...">
+                  <span>Sarah Conner</span>
+                </div>
 
-              <div class="media-body">
-                <p class="lead fw-400">Introduction</p>
-                <p><small>Article</small></p>
-              </div>
+                <div class="media media-single">
+                  <img class="avatar avatar-sm" src="../assets/img/avatar/4.jpg" alt="...">
+                  <span>Tim Hank</span>
+                </div>
 
-              <!-- <div class="media-footer"><i class="ti-angle-right text-primary"></i></div> -->
-            </a>
-          </div>
-          <div class=" hover-shadow-2  col-md-6 col-lg-6 col-sm-12">
-            <a class="media bg-white align-items-center hover-shadow-2 transition-5s" href="article-introduction.html">
-              <div class="media-left" style="border: 2px red solid;">
-                <!-- <i class="fa fa-file-text-o fs-35 text-secondary"></i> -->
-                  <img class="w-80px h-80px" src="../assets/img/login-bg.jpg" alt="...">
-                
-              </div>
-
-              <div class="media-body">
-                <p class="lead fw-400">Introduction</p>
-                <p><small>Article</small></p>
-              </div>
-
-              <!-- <div class="media-footer"><i class="ti-angle-right text-primary"></i></div> -->
-            </a>
-          </div>
-          <div class=" hover-shadow-2  col-md-6 col-lg-6 col-sm-12">
-            <a class="media bg-white align-items-center hover-shadow-2 transition-5s" href="article-introduction.html">
-              <div class="media-left" style="border: 2px red solid;">
-                <!-- <i class="fa fa-file-text-o fs-35 text-secondary"></i> -->
-                  <img class="w-80px h-80px" src="../assets/img/login-bg.jpg" alt="hayop">
-                
-              </div>
-
-              <div class="media-body">
-                <p class="lead fw-400">Introduction</p>
-                <p><small>Article</small></p>
-              </div>
-
-              <!-- <div class="media-footer"><i class="ti-angle-right text-primary"></i></div> -->
-            </a>
-          </div>
-              <!-- <div class="col-6">
-                <div class="h-40px b-1 bg-pale-secondary">1</div>
-              </div>
-
-              <div class="col-6">
-                <div class="h-40px b-1 bg-pale-secondary">2</div>
-              </div>
-
-              <div class="col-6">
-                <div class="h-40px b-1 bg-pale-secondary">3</div>
-              </div>
-
-              <div class="col-6">
-                <div class="h-40px b-1 bg-pale-secondary">4</div>
-              </div>
-
-              <div class="col-6">
-                <div class="h-40px b-1 bg-pale-secondary">5</div>
-              </div>
-
-              <div class="col-6">
-                <div class="h-40px b-1 bg-pale-secondary">6</div>
-              </div> -->
+                <div class="media media-single">
+                  <img class="avatar avatar-sm" src="../assets/img/avatar/5.jpg" alt="...">
+                  <span>Frank Camly</span>
+                </div> -->
+            </div>
         </div>
-        <div class=" col-md-4 col-md-4" style="border: 3px solid red;">
-              <div class="media media-single" style="border: 3px solid purple;">
-                <img class="avatar" src="../assets/img/avatar/1.jpg" alt="...">
-                <a class="title hover-primary text-truncate" href="#"><strong>Kermit D. Larson, John J. Wild</strong></a>
-                <a class="media-action hover-primary" href="#" data-provide="tooltip" title="Edit"><i class="ti-pencil"></i></a>
-                <a class="media-action hover-danger" href="#" data-provide="tooltip" title="Delete"><i class="ti-close"></i></a>
-              </div>
-              <div class="media media-single">
-                <img class="avatar" src="../assets/img/avatar/1.jpg" alt="...">
-                <a class="title hover-primary" href="#"><strong>Kermit D. </strong></a>
-                <a class="media-action hover-primary" href="#" data-provide="tooltip" title="Edit"><i class="ti-pencil"></i></a>
-                <a class="media-action hover-danger" href="#" data-provide="tooltip" title="Delete"><i class="ti-close"></i></a>
-              </div>
-              <div class="media media-single">
-                <img class="avatar" src="../assets/img/avatar/1.jpg" alt="...">
-                <a class="title hover-primary" href="#"><strong>Kermit D. Larson, John J. Wild</strong></a>
-                <a class="media-action hover-primary" href="#" data-provide="tooltip" title="Edit"><i class="ti-pencil"></i></a>
-                <a class="media-action hover-danger" href="#" data-provide="tooltip" title="Delete"><i class="ti-close"></i></a>
-              </div>
-              <div class="media media-single">
-                <img class="avatar" src="../assets/img/avatar/1.jpg" alt="...">
-                <a class="title hover-primary" href="#"><strong>Maryam Amiri</strong></a>
-                <a class="media-action hover-primary" href="#" data-provide="tooltip" title="Edit"><i class="ti-pencil"></i></a>
-                <a class="media-action hover-danger" href="#" data-provide="tooltip" title="Delete"><i class="ti-close"></i></a>
-              </div>
-              <div class="media media-single">
-                <img class="avatar" src="../assets/img/avatar/1.jpg" alt="...">
-                <a class="title hover-primary" href="#"><strong>Maryam Amiri</strong></a>
-                <a class="media-action hover-primary" href="#" data-provide="tooltip" title="Edit"><i class="ti-pencil"></i></a>
-                <a class="media-action hover-danger" href="#" data-provide="tooltip" title="Delete"><i class="ti-close"></i></a>
-              </div>
-              <!-- <div class="col-12">
-                <div class="h-40px b-1 bg-pale-secondary"></div>
-              </div>
+    </div>
+    <div class="col-4">
+        <div class="card">
+          <h4 class="card-title">Popular Authors</h4>
 
-              <div class="col-12">
-                <div class="h-40px b-1 bg-pale-secondary"></div>
-              </div>
+          <div class="media-list media-list-divided media-list-hover">
+            <div class="media media-single">
+              <img class="avatar avatar-sm" src="../assets/img/avatar/1.jpg" alt="...">
+              <span>Maryam Amiri</span>
+            </div>
 
-              <div class="col-12">
-                <div class="h-40px b-1 bg-pale-secondary"></div>
-              </div>
+            <div class="media media-single">
+              <img class="avatar avatar-sm" src="../assets/img/avatar/2.jpg" alt="...">
+              <span>Hossein Shams</span>
+            </div>
 
-              <div class="col-12">
-                <div class="h-40px b-1 bg-pale-secondary"></div>
-              </div>
+            <div class="media media-single">
+              <img class="avatar avatar-sm" src="../assets/img/avatar/3.jpg" alt="...">
+              <span>Sarah Conner</span>
+            </div>
 
-              <div class="col-12">
-                <div class="h-40px b-1 bg-pale-secondary"></div>
-              </div>
+            <div class="media media-single">
+              <img class="avatar avatar-sm" src="../assets/img/avatar/4.jpg" alt="...">
+              <span>Tim Hank</span>
+            </div>
 
-              <div class="col-12">
-                <div class="h-40px b-1 bg-pale-secondary"></div>
-              </div> -->
+            <div class="media media-single">
+              <img class="avatar avatar-sm" src="../assets/img/avatar/5.jpg" alt="...">
+              <span>Frank Camly</span>
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
 	<div class="card">
         <div class="card-body">
             <h1>Welcome to OLAS!</h1>
