cd643f7d75bee5f3d74a633466454ab096ead9f6
Nath Piquero <30515378+naytanyel@users.noreply.github.com>
2019-01-30 02:33:40 +0800

    Update index.php

--- application/views/OLAS/index.php
+++ application/views/OLAS/index.php
@@ -195,85 +195,74 @@
             <div class="card-pale-secondary">
                 <!-- <h4 class="card-title">Popular Authors</h4> -->
                 <div class="media-list media-list-divided media-list-hover">
-					<?php foreach($books as $book): ?>				
-						<!-- <?php print_r($book); ?>				 -->
-						<div class="media">
-							<img class="w-80px h-80px" src="<?php echo base_url("assetsOLAS/img/book/").$book['book']->Image; ?>" alt="...">						
-							<div class="media-body">
-								<h3 class="fs-24 fw-500">
-									<?php echo $book['book']->Title; ?>
-								</h3>
-								<?php foreach($book['author'] as $author): ?>
-									<h4 class="fs-18 fw-300">Author: <?php echo $author->Name; ?></h4>
-								<?php endforeach; ?>							
-								<!-- <?php foreach($book['genre'] as $genre): ?>
-									<p class="fs-14 fw-100">Genre: <?php echo $genre->Name; ?></p>
-								<?php endforeach; ?>
-								<?php foreach($book['subject'] as $subject): ?>
-									<p class="fs-14 fw-100">Subject: <?php echo $subject->Name; ?></p>
-								<?php endforeach; ?> -->
-							</div>							
-							<?php if($this->session->has_userdata('isPatron')): ?>		
-
-								<!-- Check if room use only -->
-								<?php if($book[0]->IsRoomUseOnly == 0): ?>
-
-									<!-- Check availabillity of the book -->
-									<?php if($book[0]->IsAvailable == 1): ?>
-
-										<!-- Check if book is reserved -->
-										<?php if($book['reservation']['IsReserved'] == 1): ?>
-											<!-- <?php print_r($book['reservation']); ?>
-											<?php echo $this->session->userdata('patronId'); ?> -->
-											<!-- Check if book is reserved by patron currently logged -->
-											<?php if($book['reservation']['PatronId'] == $this->session->userdata('patronId')): ?>											
-												<a class="media-action hover-primary" href="#" data-provide="tooltip" 	title="You have already reserved this book"><i class="fa fa-home fa-2x" style="color:#48b0f7"></i></a>
-											<?php else: ?>
-												<a class="media-action hover-primary" href="#" data-provide="tooltip" 	title="This book is already reserved by someone else"><i class="fa fa-home fa-2x" style="color:#48b0f7"></i></a>
-											<?php endif; ?>
-										
-										<?php else: ?>
-											<!-- Add to bookbag -->
-											<a class="media-action hover-primary" onclick = "Bookbag.add('<?php echo $book[0]->AccessionNumber; ?>','<?php echo $book[0]->ISBN; ?>');" href="#" data-provide="tooltip" title="Add to Book Bag"><i class="fa fa-plus fa-2x" style="color:#48b0f7"></i></a>
-										<?php endif; ?>
-									<?php else: ?>
-										<a class="media-action hover-primary" href="#" data-provide="tooltip" title="Book is not present at the library at the moment"><i class="fa fa-disable fa-2x" style="color:#48b0f7"></i></a>
-									<?php endif; ?>
-
-								<?php else: ?>
-									<!-- IsRoomUseOnly -->
-									<a class="media-action hover-primary" href="#" data-provide="tooltip" title="This book is for room use only"><i class="fa fa-home fa-2x" style="color:#48b0f7"></i></a>									
-								<?php endif; ?>
-
-							<?php endif; ?>
-							<a class="media-action hover-primary" href="<?php echo base_url('Book/View/'.$book[0]->AccessionNumber); ?>" data-provide="tooltip" title="More information about this book"><i class="fa fa-eye fa-2x" style="color:#48b0f7"></i></a>
-						</div>	
-					<?php endforeach; ?>
-				</div>				
+                <?php foreach($authors as $author): ?>
+                    <div class="media media-single">
+                        <img class="avatar avatar-sm" src="<?php echo base_url("assets/img/avatar/1.jpg"); ?>" alt="...">
+                        <span><?php echo $author->Name; ?></span>
+                    </div>          
+                <?php endforeach; ?>
+                </div>
             </div>
-        </div>
-		<div class="col-lg-4 col-md-4 col-sm-12">
-			<div class="card">
-				<h4 class="card-title">Popular Authors</h4>
-				<div class="media-list media-list-divided media-list-hover">
-				<?php foreach($authors as $author): ?>
-					<div class="media media-single">
-						<img class="avatar avatar-sm" src="<?php echo base_url("assets/img/avatar/1.jpg"); ?>" alt="...">
-						<span><?php echo $author->Name; ?></span>
-					</div>			
-				<?php endforeach; ?>
-				</div>
-			</div>
-			</div>
-		</div>
-    </div>	
+            <!-- </div> -->
+                    <!-- <div class="row mini-list-authors">
+                        <div class="col-lg-12">
+                            <div class="section-title">
+                                <h2>Popular Authors</h2>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-6 author">
+                            <div class="author-photo">
+                                <a href="/author/597/books" class="text-center"><img src="<?php echo base_url("assets/img/avatar/1.jpg"); ?>" alt="J.K. Rowling " class="rounded-circle"></a>
+                            </div>
+                            <div class="author-info">
+                                <div class="author-name">
+                                    <a href="/author/597/books"> J.K. Rowling</a>
+                                </div>
+                                <div class="author-books">1 books</div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-6 author">
+                            <div class="author-photo">
+                                <a href="/author/602/books" class="text-center"><img src="<?php echo base_url("assetsOLAS/img/book/comingsoon.png"); ?>" alt="Шерих Дмитрий" class="rounded-circle"></a>
+                            </div>
+                            <div class="author-info">
+                                <div class="author-name">
+                                    <a href="/author/602/books">Дмитрий Шерих</a>
+                                </div>
+                                <div class="author-books">1 books</div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-6 author">
+                            <div class="author-photo">
+                                <a href="/author/609/books" class="text-center"><img src="<?php echo base_url("assetsOLAS/img/book/comingsoon.png"); ?>" class="rounded-circle"></a>
+                            </div>
+                            <div class="author-info">
+                                <div class="author-name">
+                                    <a href="/author/609/books"> Стивен Кови</a>
+                                </div>
+                                <div class="author-books">1 books</div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-6 author">
+                            <div class="author-photo">
+                                <a href="/author/615/books" class="text-center"><img src="/resources/images/noavatar.png" alt="Behrouz A. Forouzan " class="rounded-circle"></a>
+                            </div>
+                            <div class="author-info">
+                                <div class="author-name">
+                                    <a href="/author/615/books"> Behrouz A. Forouzan</a>
+                                </div>
+                                <div class="author-books">1 books</div>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
 </div>
 <script>
-	$(document).ready(function(){				
-		$('#welcome').fadeOut({			
-			complete: function(){
-				$(this).html("Welcome to <text style='color:#ff4e4e; font-family: Century Gothic; letter-spacing: 5px;'>O<text style='color:#000000; font-weight: bold;'>L</text>AS</text>!").fadeIn("slow");
-			}
-		});		
-	});
+    $(document).ready(function(){               
+        $('#welcome').fadeOut({         
+            complete: function(){
+                $(this).html("Welcome to <text style='color:#ff4e4e; font-family: Century Gothic; letter-spacing: 5px;'>O<text style='color:#000000; font-weight: bold;'>L</text>AS</text>!").fadeIn("slow");
+            }
+        });     
+    });
 </script>
