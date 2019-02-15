91c70345259ad4444a24f5d147d78e646d5465a8
MARKOS\User <johnmarksena@gmail.com>
2019-02-15 15:27:51 +0800

    Messenger

--- application/views/OLAS/index.php
+++ application/views/OLAS/index.php
@@ -255,6 +255,8 @@
                     </div> -->
                 </div>
 </div>
+<div class="fb-customerchat" page_id="342927939764635"></div>
+</div>
 <script>
     $(document).ready(function(){               
         $('#welcome').fadeOut({         
@@ -263,4 +265,21 @@
             }
         });     
     });
+
+    window.fbAsyncInit = function() {
+        FB.init({
+        appId            : '622129968224413',
+        autoLogAppEvents : true,
+        xfbml            : true,
+        version          : 'v2.12'
+        });
+    };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 </script>
